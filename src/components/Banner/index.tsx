cf9ddbdfec0a916b0a4419b4a3f1289946825d9e
Narek Petrosyan <narek.petrosyan@softconstruct.com>
2024-12-25 18:43:19 +0400

    add title in banner component and add title and description for projects page

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -5,19 +5,26 @@ import classNames from "classnames";
 
 interface Props {
   classname?: string;
+  title?: string;
 }
 
-const Banner: React.FC<Props> = ({ classname }) => {
+const Banner: React.FC<Props> = ({ classname, title }) => {
   const translation = useTranslations("common");
 
   return (
     <div
       title={translation("bannerImageAlt")}
       className={classNames(
-        "bg-[url('/images/banner.webp')] w-full h-[292px] rounded-t-[15px] bg-[#2eca7f] bg-cover bg-center bg-blend-color-burn",
+        "bg-[url('/images/banner.webp')] w-full h-[292px] rounded-t-[15px] bg-[#2eca7f] bg-cover bg-center bg-blend-color-burn flex items-center justify-center",
         classname
       )}
-    />
+    >
+      {title && (
+        <h1 className="text-[#FFFFFF] text-[32px] tracking-wide font-bold">
+          {title}
+        </h1>
+      )}
+    </div>
   );
 };
 
