59025047538061d8d2ba0cceb0945fcb1686313b
Narek Petrosyan <narek.petrosyan@softconstruct.com>
2025-01-03 12:33:39 +0400

    style fixing

--- src/app/[locale]/resume/page.tsx
+++ src/app/[locale]/resume/page.tsx
@@ -14,7 +14,7 @@ const Resume: React.FC = () => {
   return (
     <Container classname="my-[20px]">
       <Banner classname="!h-[138px]" title={commonTranslation("resume")} />
-      <div className="pt-[30px] shadow rounded-b-[15px] p-[40px] bg-[#FFFFFF] flex flex-col gap-[50px]">
+      <div className="shadow rounded-b-[15px] p-[30px] bg-[#FFFFFF] flex flex-col gap-[50px]">
         <Achievements />
         <ResumeList />
         <Educations />
